8d20a3040a4b14c9c45b07ba03247551031f44b8
one-rose <100477604+one-rose@users.noreply.github.com>
2022-02-27 02:02:37 +0900

    Add files via upload

---
+++ 3단계.c
@@ -0,0 +1,13 @@
+//고양이 출력하기
+#include <stdio.h>
+int main(void)
+{
+ printf("\\    /\\\n");
+ printf(" )  ( ')\n");
+ printf("(  /  )\n");
+ printf(" \\(__)|\n");
+    return 0;
+}
+
+
+
---
+++ 5단계.c
@@ -0,0 +1,11 @@
+//A와 B를 입력받아 A+B 출력하기
+
+#include<stdio.h>
+int main (void){
+    int num1 , num2;
+    scanf("%d %d",&num1 ,&num2);
+    printf("%d",num1+num2);
+    
+    return 0;
+    
+}
