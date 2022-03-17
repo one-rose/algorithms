daf9d291bb8d362a1cac036246a15dbc3fd82663
one-rose <100477604+one-rose@users.noreply.github.com>
2022-03-18 01:33:03 +0900

    Add files via upload

---
+++ 기본문제풀이(반복문)_두수사이숫자_문제.js
@@ -0,0 +1,34 @@
+/*** 2. 두 수 사이 숫자 ***/
+
+/* user code */
+// x = 3, y = 7
+function answer(x, y) {
+  let result = [];
+
+    // x < y
+  if (x > y) {
+    let t = x;
+    x = y;
+    y = t;
+  }
+
+  for (let i = x; i <= y; i++) {
+    result.push(i);
+  }
+
+  return result;
+}
+
+/* main code */
+let input = [
+  // TC: 1
+  [3, 7],
+  // TC: 2
+  [8, 3],
+  // TC: 3
+  [12, 10],
+];
+for (let i = 0; i < input.length; i++) {
+  process.stdout.write(`#${i + 1} `);
+  console.log(answer(input[i][0], input[i][1]));
+}
